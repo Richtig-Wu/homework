4eaaaf57e3af014e713ee60c611163dc56c419f7
QuanZhong WU <wuquanzhong@yahoo.com>
2017-10-23 09:06:24 +0800

    id

---
+++ lianxi/jd/js.js
@@ -0,0 +1,19 @@
+// function abc(a){
+//   var arr =[];
+//   for (let i = 0; i < a; i++) {
+//     if (i < 2) {
+//       arr.push(i);
+//     }else {
+//       arr.push(arr[i-2]+arr[i-1]);
+//     }
+//   }
+//   return arr;
+// }
+// console.log(abc(8)); //0,1,1,2,3,5,8,13....
+
+function abc(a){
+  var arr = [];
+  arr.push(iterable.reduce((p, c) => {
+
+  }, initial);)
+}
